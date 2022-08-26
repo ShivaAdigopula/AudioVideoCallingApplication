09c60929d300b9a5919ba368cb18180d31e87ef0
Shiva Adigopula <vskrao.adigopula@gmail.com>
2022-08-26 23:22:57 +0400

    some alignment changes

--- video-calling-client/src/App.js
+++ video-calling-client/src/App.js
@@ -6,7 +6,7 @@ import { VideoWrapper } from "./Components/VideoWrapper";
 function App() {
   return (
     <div className="flex flex-col justify-center items-center w-screen h-screen">
-      <div className=" mx-10 my-5 h-1/2 w-1/2 border-2 border-blue-500">
+      <div className=" mx-10 my-5 h-1/2 w-screen  border-2 border-blue-500 sm:w-1/2 md:1/2 lg:w-1/2 xl:w-1/2">
         <VideoWrapper />
       </div>
       <div className="mx-10 my-5">
--- video-calling-client/src/Components/VideoWrapper.jsx
+++ video-calling-client/src/Components/VideoWrapper.jsx
@@ -1,21 +1,14 @@
 import React, { useContext } from "react";
 import { SocketContext } from "../Context";
 export const VideoWrapper = () => {
-  const {
-    name,
-    callAccepted,
-    myVideo,
-    userVideo,
-    callEnded,
-    stream,
-    call,
-  } = useContext(SocketContext);
+  const { name, callAccepted, myVideo, userVideo, callEnded, stream, call } =
+    useContext(SocketContext);
 
   return (
     <>
-      <div className="flex w-full h-full justify-center items-center">
+      <div className="flex flex-col w-full h-full justify-center items-center sm:flex-row md:flex-row lg:flex-row xl:flex-row">
         {stream && (
-          <div className="flex flex-col justify-center items-center h-full  w-1/2">
+          <div className="flex flex-col justify-center items-center h-full  w-screen sm:w-1/2 md:w-1/2 lg:w-1/2 xl:w-1/2">
             <div>
               <video
                 playsInline
@@ -29,7 +22,7 @@ export const VideoWrapper = () => {
         )}
 
         {!callEnded && callAccepted && (
-          <div className="flex flex-col justify-center items-center h-full  w-1/2">
+          <div className="flex flex-col justify-center items-center h-full  w-screen sm:w-1/2 md:w-1/2 lg:w-1/2 xl:w-1/2">
             <div>
               <video
                 playsInline
